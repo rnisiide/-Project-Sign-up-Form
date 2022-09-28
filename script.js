56d9e5f01a4565059d43f24882c830c4f5fae9b0
Ricardo Nisiide <rnisiide@gmail.com>
2022-09-27 22:39:25 -0300

    finished form and css. added JS to validate matching passwords

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+let pswd = document.getElementById("password");
+let confirmpswd = document.getElementById("confirmpassword");
+let pswdspan = document.querySelector(".pswdmatch")
+
+
+
+pswd.addEventListener ("blur", (event) => {
+    pswdverify();
+}) 
+
+confirmpswd.addEventListener ("blur", () => {
+    pswdverify()
+})
+
+function pswdverify() {
+    if (pswd.value != confirmpswd.value) {
+        pswd.style.borderColor = "#b28dd8";
+        confirmpswd.style.borderColor = "#b28dd8";
+        pswdspan.style.opacity = "1";
+    } else {
+        pswdspan.style.opacity = "0"
+        pswd.style.borderColor = "";
+        confirmpswd.style.borderColor = "";
+    }
+
+}
